4516e222a0c524c5ef9cfa169a3226e710f95207
Dhanidotini <apokta2006@gmail.com>
2025-08-03 15:59:05 +0700

    adding link in breadcrumb 'anime'

--- resources/views/components/partials/breadcrumb.blade.php
+++ resources/views/components/partials/breadcrumb.blade.php
@@ -1,10 +1,13 @@
 <div>
     <div class=" text-sm text-gray-400 mt-2 mb-4 mx-2 [&_a]:hover:underline [&_a]:hover:text-white">
         <a href="{{ route('home') }}">Home</a>
-        > Anime >
+        >
+        <a href="{{ route('anime.index') }}">Anime</a>
+        >
         <a href="{{ route('anime.show', $anime->slug) }}">{{ $anime->title }}</a>
+        >
         @isset($episode)
-            > Episode {{ $episode->episode }}
+            Episode {{ $episode->episode }}
         @endisset
     </div>
 </div>
